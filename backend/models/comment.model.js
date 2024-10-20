45d88c3f5e4a770dd2bb0bf044a87645703add9c
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-20 13:31:42 +0530

    Add comments model

---
+++ backend/models/comment.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    author: {
+      // who comment in your show user id
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      // which post comment and which id comment
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
