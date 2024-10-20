733ab4093ff0dab52387504e83d5207d32688a33
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-20 17:57:18 +0530

    Add middleware isAuthenticated means check

---
+++ backend/middlewares/isAuthenticated.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+
+
+export const isAuthenticated = async (req, res, next) => {
+ try {
+    const token = req.cookies.token; //  take token from user.controlles
+    if(!token){
+        return res.status(401).json({
+            massag: 'User not authenticated!',
+            success: false,
+        })
+    }
+    const decode = await jwt.verify(token, process.env.SECRET_KEY);
+     if (!decode) {
+        return res.status(401).json({
+            massag: 'Invalid',
+            success: false,
+        })
+     }
+     req.id = decode.userId;
+     next()
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
