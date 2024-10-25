882f562b98b19588634d42ca8e61cbcc60ac4613
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-10-25 12:26:05 +0530

    Add Post  Optized and add post

--- frontend/src/Components/Post/Post.jsx
+++ frontend/src/Components/Post/Post.jsx
@@ -11,6 +11,9 @@ import img6 from "../../assets/6.jpg";
 import img8 from "../../assets/8.jpg";
 import { Button, Menu, MenuItem } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import { FaRegHeart } from "react-icons/fa";
+import { FaRegComment } from "react-icons/fa";
+import { FaPaperPlane, FaBookmark } from "react-icons/fa";
 
 function Post() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -22,8 +25,8 @@ function Post() {
     setAnchorEl(null);
   };
   return (
-    <div className="post  h-full  grid-cols-1 grid-rows-5 gap-4 ">
-      <div className=" flex justify-center items-center m-2 ">
+    <div className="post  grid-cols-1 grid-rows-5 gap-4 z-5 h-full items-start  bg-pink-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-200 ">
+      <div className=" flex justify-center  items-center m-2 cursor-pointer ">
         <Stack direction="row" spacing={2}>
           <div>
             <Avatar alt="Remy Sharp" src={img1} />
@@ -56,56 +59,68 @@ function Post() {
         </Stack>
       </div>
 
-      <div className="flex items-center justify-between ">
-        <div className="flex justify-start items-center m-3 w-full ">
-          <Avatar alt="Remy Sharp" src={img1} />
-          <div>
-            <p className=" flex justify-center m-2 text-lg font-medium ">
-              sandeep <b className="flex justify-between items-center">.</b>10h
-            </p>
-            <p className=" m-2 mt-[-2vh] text-sm">Original music</p>
+      <div className=" justify-center m-[1rem] ">
+        <div className="h-full w-full items-start  bg-pink-250 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-200">
+          <div className="flex justify-between items-center">
+            <Avatar alt="Remy Sharp" src={img1} />
+            <div className="grid justify-start items-start ml-[-17rem] ">
+              <p className=" flex justify-center m-2 text-lg font-medium  ">
+                sandeep
+                <b>.</b>
+                <p className=" text-[small] font-[serif] "> 10h</p>
+              </p>
+              <p className=" m-2 mt-[-2vh] text-sm  ">Original music</p>
+            </div>
+            <div className="flex justify-start items-center ">
+              <Button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <MenuIcon />
+              </Button>
+              <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                  "aria-labelledby": "basic-button",
+                }}
+              >
+                <div className="grid justify-center h-full w-full items-start  bg-pink-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-200">
+                  <MenuItem onClick={handleClose}>Report</MenuItem>
+                  <MenuItem onClick={handleClose}>Unfollow</MenuItem>
+                  <MenuItem onClick={handleClose}>Add to Favavoritest</MenuItem>
+                  <MenuItem onClick={handleClose}>Go to post</MenuItem>
+                  <MenuItem onClick={handleClose}>Shere to...</MenuItem>
+                  <MenuItem onClick={handleClose}>Copy link</MenuItem>
+                  <MenuItem onClick={handleClose}>Embed</MenuItem>
+                  <MenuItem onClick={handleClose}>About this account</MenuItem>
+                  <MenuItem onClick={handleClose}>Cancle</MenuItem>
+                </div>
+              </Menu>
+            </div>
           </div>
-
-          <div>
-            <Button
-              id="basic-button"
-              aria-controls={open ? "basic-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-            >
-              <MenuIcon />
-            </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                "aria-labelledby": "basic-button",
-              }}
-            >
-              <div className="h-full w-full bg-pink-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100">
-                <MenuItem onClick={handleClose}>Report</MenuItem>
-                <MenuItem onClick={handleClose}>Unfollow</MenuItem>
-                <MenuItem onClick={handleClose}>Add to Favavoritest</MenuItem>
-                <MenuItem onClick={handleClose}>Go to post</MenuItem>
-                <MenuItem onClick={handleClose}>Shere to...</MenuItem>
-                <MenuItem onClick={handleClose}>Copy link</MenuItem>
-                <MenuItem onClick={handleClose}>Embed</MenuItem>
-                <MenuItem onClick={handleClose}>About this account</MenuItem>
-                <MenuItem onClick={handleClose}>Cancle</MenuItem>
-              </div>
-            </Menu>
+          <div className="grid justify-center h-full w-full items-start  bg-pink-180 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-200">
+            <img
+              className=" rounded-sm my-2 m-full aspect-square object-cover "
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_drZoS4Q2mT2kF-foAfDwu56w2WwrIT-Bvg&s"
+              alt=""
+            />
           </div>
-        </div>
-        <div className="flex item-center gap-2">
-          
+          <div className=" flex items-center justify-between  my-2 ml-2 ">
+            <div className=" flex items-center gap-5 cursor-pointer ">
+             <FaRegHeart className="cursor-pointer hover:text-red-500"/>
+               <FaRegComment className="cursor-pointer hover:text-gray-500   " />
+              <FaPaperPlane className="cursor-pointer hover:text-gray-500   " />
+            </div> <FaBookmark className="mr-2 cursor-pointer hover:text-gray-500"/>
+          </div>
+         
         </div>
       </div>
-
-
-
     </div>
   );
 }
